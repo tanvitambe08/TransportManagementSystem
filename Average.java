b5106172d17c13984c17a04e7256a8ea706e2630
tanvitambe08 <103626271+tanvitambe08@users.noreply.github.com>
2024-08-16 17:23:08 +0530

    Add files via upload
    This file calculates average of 2 nos in transport management system.

---
+++ Average.java
@@ -0,0 +1,15 @@
+package transportmanagementsys;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 8;
+		int b = 10;
+		int c;
+		c = (a+b)/2;
+		System.out.println("Average of 2 nos. is : " + c);
+
+	}
+
+}
